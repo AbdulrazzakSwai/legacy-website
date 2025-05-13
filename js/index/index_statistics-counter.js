0f38dba61a672c30246bd5f424fbda33ee79bc0d
Abdulrazzak Ahmed Swai <abdulrazzakswai2004@gmail.com>
2025-05-13 11:33:40 +0400

    Update index_statistics-counter.js

--- js/index/index_statistics-counter.js
+++ js/index/index_statistics-counter.js
@@ -19,7 +19,7 @@ export function handleScrollAnimation() {
       !counter.classList.contains('started')
     ) {
       counter.classList.add('started');
-      animateCounter(counter, 1200);
+      animateCounter(counter, 1100);
     }
   });
 }
